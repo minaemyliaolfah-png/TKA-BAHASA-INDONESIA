8321ad5f073905a5ce27d9d43afecc8fd943f7d6
minaemyliaolfah-png <minaemyliaolfah@gmail.com>
2026-09-19 09:11:06 +0700

    Delete dist directory

--- dist/js/config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-  SUPABASE_URL: "https://ermgrhehunrbyifqdwcj.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVybWdyaGVodW5yYnlpZnFkd2NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3Mjg3NDUsImV4cCI6MjEwNTMwNDc0NX0.763lVpVSpkVPu9ZzD5buC8cQZ-5puj_ohCk-a_z_U50",
-  APP_NAME: "TKA Cerdas",
-};
